059673c104d39f6fd1da0f4bafaca52114d86ab9
Kyaw-Thiha <k.thiha10.mail@gmail.com>
2025-03-11 11:30:43 -0400

    Implementing the react flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useMemo, useState } from 'react'
 import './App.css'
+import { ReactFlow, Controls, Background, Node, Edge } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import expandableNode from './expandableNode';
+
+const edges: Edge[] = [{ id: '1-2', source: '1', target: '2', sourceHandle: 'bottom-center' }];
+
+const nodes: Node[] = [
+  {
+    id: '1',
+    type: 'expandableNode',
+    data: { label: 'Hello' },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: '2',
+    type: 'expandableNode',
+    data: { label: 'World' },
+    position: { x: 100, y: 100 },
+  },
+];
 
 function App() {
   const [count, setCount] = useState(0)
+  const nodeTypes = useMemo(() => ({ expandableNode: expandableNode }), []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bg-red-300">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen w-screen'>
+      <ReactFlow colorMode='dark' nodes={nodes} edges={edges} nodeTypes={nodeTypes}>
+        <Background />
+        {/* <Controls /> */}
+      </ReactFlow>
+    </div>
   )
 }
 
